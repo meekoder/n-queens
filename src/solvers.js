2a2d84737ddf8598e20f98ed7538fb2d2d2912da
markwgquintos <markwgquintos@yahoo.com>
2020-10-15 18:08:25 -0700

    Still solving findNQueensSolution. Completed the rest

--- src/solvers.js
+++ src/solvers.js
@@ -36,10 +36,100 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+
+  let board = new Board({n: n});
+
+  let boardTree = function(row) {
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+
+      if (!board.hasAnyRooksConflicts()) {
+        boardTree(row + 1);
+      }
+
+      board.togglePiece(row, i);
+    }
+
+  };
+  boardTree(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // console.log(n);
+  // const boardObj = new Board({n: n});
+  // let board = boardObj.rows();
+  // var listOfTrees = new Set();
+  // var solutionCount = listOfTrees.size;
+  // var listOfSpaces = {};
+
+  // var columns = new Set();
+  // for (let i = 0; i < n; i++) {
+  //   columns.add(i);
+  // }
+
+  // for (let i = 0; i < n; i++) {
+  //   listOfSpaces[i] = columns;
+  // }
+  // //tree func with default param (empty tree or parent tree, indexes = 0, or parent indexes)
+  // const Tree = (board, listOfSpaces) => {
+  //   console.log(board);
+  //   console.warn(listOfTrees);
+  //   for (let row in listOfSpaces) {
+  //     console.log(listOfSpaces, listOfSpaces[row]);
+  //     for (let column of listOfSpaces[row]) {
+  //       board[row][column] = 1;
+  //       delete listOfSpaces[row];
+
+  //       for (let key in listOfSpaces) {
+  //         if (listOfSpaces[key].has(column)) {
+  //           listOfSpaces[key].delete(column);
+  //         }
+  //       }
+
+  //       if (Object.keys(listOfSpaces).length === 0) {
+  //         let strBoard = JSON.stringify(board);
+  //         listOfTrees.add(strBoard);
+  //       } else {
+  //         Tree(board, listOfSpaces);
+  //       }
+  //     }
+  //   }
+  // };
+
+  // Tree(board, listOfSpaces);
+  // //iterate through each space to put first piece
+  // //create tree obj => new board
+  // //recurse tree with parent tree and indexes
+  // console.log('Number of solutions for ' + n + ' rooks:', listOfTrees.size);
+  // return listOfTrees.size;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -66,7 +156,22 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  let solutionCount = 0; //fixme
+  const board = new Board({ n: n });
+  const boardTree = (row) => {
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+    for (let i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      if (!board.hasAnyQueensConflicts()) {
+        boardTree(row + 1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+  boardTree(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
